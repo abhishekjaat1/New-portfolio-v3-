30d16746949375cb89dd14fe6d9a875f1306a857
abhishekjaat1 <yagamingyt1@gmail.com>
2025-08-31 11:25:38 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Portfolio from './Portfolio';
+
+function App() {
+  return (
+    <div style={{ backgroundColor: '#0a0a0a', minHeight: '100vh', color: 'white' }}>
+      <Portfolio />
+    </div>
+  );
+}
+
+export default App;
